f81155bc83339865428c976a3c50a9b316c01626
Astn <austin.w.harris@gmail.com>
2014-10-05 15:37:17 -0600

    update assembly info

--- MDLd/Properties/AssemblyInfo.cs
+++ MDLd/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MDLd")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MDL Decompiler")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Allegiance Zone")]
 [assembly: AssemblyProduct("MDLd")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
